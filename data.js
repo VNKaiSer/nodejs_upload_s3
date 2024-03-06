867f003e653c9db5f8e4640346a62120e8a399d1
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-03-06 14:51:48 +0700

    init

---
+++ data.js
@@ -0,0 +1,18 @@
+let customers = [
+    {
+      id: 1,
+      email: "isidro_von@hotmail.com",
+      name : "VNKaiser",
+      company: "Hilll, Mayert and Wolf",
+      country: "Switzerland"
+    },
+    {
+      id: 2,
+      email: "frederique19@gmail.com",
+      name : "David",
+      company: "Stokes-Reichel",
+      country: "Democratic People's Republic of Korea"
+    }
+  ]
+
+module.exports = customers;
---
+++ index.js
@@ -0,0 +1,146 @@
+const express = require('express'); 
+const app = express(); 
+let customers = require('./data.js')
+const bodyParser = require('body-parser');
+const AWS = require('aws-sdk')
+require('dotenv').config();
+const path = require('path');
+const multer = require('multer');
+
+// middlewares
+app.use(express.json());
+app.use(express.static('./views'))
+
+// views
+app.set('view engine', 'ejs')
+app.set('views', './views')
+const PORT = 3000;  
+
+// Config AWS
+
+AWS.config.update({
+    region : process.env.REGION,
+    accessKeyId : process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+})
+
+const s3 = new AWS.S3();
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = 'customer'
+
+// Config Multer
+
+const storage = multer.memoryStorage({
+    destination(req, file, callback){
+        callback(null,'')
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: {fieldSize: 2000000},
+    fileFilter(req, file, callback){
+        checkFileType(file, callback)
+    }
+})
+
+function checkFileType(file, callback){
+    const fileTypes = /jpeg|jpg|png|gif/
+
+    const extname = fileTypes.test(path.extname(file.originalname).toLocaleLowerCase())
+    const minetype = fileTypes.test(file.minetype);
+
+    if(extname && minetype){
+        return callback(null, true)
+    }
+
+    return callback('Error: Image Error')
+}
+
+// Route
+
+app.get('/', async (req, res) => {
+    try{
+        const params = {TableName : tableName}
+        const customers = await dynamoDB.scan(params).promise();
+       
+        return res.render('index', {customers})
+    } catch{
+        console.log('Error no fetching data from DynamoDB')
+        return res.status(500).send('500 - Internal Server Error')
+    }
+    
+})
+
+app.post('/', upload.single('image'),async (req, res) =>{
+    console.log(req.file)
+    try {
+        const params = {TableName : tableName}
+        const customers = await dynamoDB.scan(params).promise();
+        
+        
+        const id = 'SP' + (customers.Count + 1);
+        
+        const name = req.body.name;
+        const email =  req.body.email;
+        const company = req.body.company;
+        const country = req.body.country;
+        // console.log(req.file)
+        const image = req.file.originalname.split('.');
+        
+        const fileType = image[image.length -1];
+        const filePath = `${id + Date.now().toString()}.${fileType}`;
+        
+
+        const paramsS3 = {
+            Bucket : process.env.S3_BUCKET_NAME,
+            Key : filePath,
+            Body : req.file.buffer,
+            ContentType : req.file.mimetype
+        }
+
+        
+        s3.upload(paramsS3, async(err, data) => {
+            if(err) {
+                console.error('error=', err)
+                return res.status(500).send('500 - Internal Server Error')
+            } else{
+                const imageUrl = data.Location;
+                const paramsDynamoDb = {
+                    TableName : tableName,
+                    Item : {
+                        id,
+                        name,
+                        email,
+                        company,
+                        country,
+                        image : imageUrl
+                    }
+                }
+
+                await dynamoDB.put({
+                    paramsDynamoDb
+                }).promise()
+
+                return res.redirect('/')
+            }
+
+        })
+    } catch (error) {
+        
+    }
+}) 
+
+
+
+app.post('/delete', (req, res) => {
+    const dataDelete  = Object.keys(req.body)
+    customers= customers.filter(i => !dataDelete.includes(i.id+""))
+    console.log(customers)
+
+   return res.redirect("/")
+})
+
+app.listen(PORT, () => {   
+    console.log(`Example app listening on port ${PORT}!`) 
+});
